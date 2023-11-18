fa65700b869e4753debe63672aa27f0d35d36381
akhilesh <akhileshkunnath17@gmail.com>
2023-11-18 23:01:19 +0530

    create update api route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to db")
-}).catch((error)=>{
+}).catch((err)=>{
     console.log(err)
 });
 
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000!!!");
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,112 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import  { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import { useSelector } from 'react-redux';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
+import { app } from '../firebase';
+import { updateUserStart, updateUserSuccess,updateUserfailure } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 const Profile = () => {
+    const fileRef = useRef(null);
+
     const {currentUser}= useSelector((state)=>(state.user));
+  
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+   const[formData, setFormData] = useState({});
+   const dispatch = useDispatch();
+   
+  
+
+    useEffect(()=>{
+        if(file) {
+            handleFileUpload(file);
+
+        }
+
+    },[file]);
+    const handleFileUpload = (file)=>{
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+
+        uploadTask.on("state_changed",
+        (snapshot)=> {
+            const progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+            setFilePerc(Math.round(progress));
+        },
+        (error)=>{
+            setFileUploadError(true);
+
+        },
+        ()=>{
+            getDownloadURL(uploadTask.snapshot.ref).then
+            ((downloadURL)=>
+                setFormData({...formData, avatar: downloadURL})
+
+            );
+        }
+        );
+
+    };
+
+    const handleChange = (e)=> {
+        setFormData({...formData, [e.target.id]: e.target.value})
+    };
+
+    const handleSubmit = async (e)=>{
+        try {
+            dispatch(updateUserStart());
+
+            const res = await fetch(`/api/user/update/${currentUser._id}`,{
+
+                method: POST,
+                headers:{
+                    "content-type": "application/json",
+                 },
+                body: JSON.stringify(formData),
+
+
+            });
+            const data = await res.json();
+            if(data.success== false){
+                dispatch(updateUserfailure(data.message));
+                return;
+            }
+            dispatch(updateUserSuccess(data));
+            
+        } catch (error) {
+            dispatch(updateUserfailure(error.message));
+
+            
+        }
+    }
   return (
     <div className='p-3 max-w-lg mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-         <form className='flex flex-col gap-4'>
-            <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 ' src={currentUser.avatar} alt="Profile" />
-            <input type="text" placeholder='username' id="username" className='border p-3 rounded-lg '/>
-            <input type="email" placeholder='email' id="email" className='border p-3 rounded-lg '/>
-            <input type="text" placeholder='password' id="password" className='border p-3 rounded-lg '/>
+         <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+           <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
+            <img onClick={()=>fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 ' src={formData.avatar||currentUser.avatar} alt="Profile" />
+           <p className='text-center'>
+            {
+                fileUploadError ? (
+                    <span className='text-red-700'>Error image upload(image must be 2MB)</span>
+                ) : filePerc > 0 && filePerc < 100 ? (
+                    <span className='text-slate-700'>{`uploading${filePerc}%`}</span>
+                ) : filePerc === 100 ? (
+                    <span className='text-yellow-500'>Image successfully uploaded</span>
+                ) : (
+                    ''
+                )
+            }
+            
+           </p>
+            <input type="text" placeholder='username' defaultValue={currentUser.username} onChange={handleChange}  id="username" className='border p-3 rounded-lg '/>
+            <input type="email" placeholder='email' defaultValue={currentUser.email } onChange={handleChange} id="username" id="email" className='border p-3 rounded-lg '/>
+            <input type="text" placeholder='password' onChange={handleChange} id="password" className='border p-3 rounded-lg '/>
          <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:placeholder-opacity-95 disabled:opacity-80'>Update</button>
          </form>
          <div className='flex justify-between mt-5'>
