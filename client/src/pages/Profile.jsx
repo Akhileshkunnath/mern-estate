ae8f3014abcdbf9fbec3f8489a839ccf0a612ece
akhilesh <akhileshkunnath17@gmail.com>
2023-11-20 18:07:43 +0530

    complete ui delete listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -144,6 +144,29 @@ const Profile = () => {
             setShowListingsError(true);
             
         }
+    };
+
+    const handleListingDelete = async(listingId)=>{
+        try {
+               const res = await fetch(`/api/listing/delete/${listingId}`,{
+                method:"DELETE",
+
+               });
+               const data = await res.json();
+               if(data.success === false) {
+                console.log(data.message);
+                return;
+               }
+
+               setUserListings((prev)=> prev.filter((listing)=> listing._id !== listingId));
+
+
+        } catch (error) {
+            
+            console.log(error.message);
+            
+        }
+
     }
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -183,7 +206,7 @@ const Profile = () => {
        <button onClick={handleShowListings}  className='text-green-700 w-full '>Show Listings</button>
     <p className='text-red-700 mt-5'>{showListingsError ? "Error showing listings" : ""}</p>
        
-       {userListings && userListings.length > 0 &&
+       {userListings && userListings.length > 0 && 
        <div className=" flex flex-col gap-4">
         <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
       {  userListings.map((listing)=>(
@@ -195,7 +218,7 @@ const Profile = () => {
                     <p className=' '>{listing.name} </p>
                 </Link>
                 <div className='flex flex-col items-center'>
-                    <button className='text-red-700 uppercase'>Delete</button>
+                    <button onClick = {()=>handleListingDelete(listing._id)}className='text-red-700 uppercase'>Delete</button>
                     <button className='text-green-700 uppercase'>Edit</button>
 
 
