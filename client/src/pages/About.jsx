b04c05b78f83118b3340a12fc235458125bb1f27
akhilesh <akhileshkunnath17@gmail.com>
2023-11-21 18:57:07 +0530

    add pre installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About AK Estate</h1>
+      <p className='mb-4 text-slate-700'>
+      One of the largest initial real estate deals in history known as the "Louisiana Purchase" happened in 1803 when the Louisiana Purchase Treaty was signed. This treaty paved the way for western expansion and made the U.S. the owners of the "Louisiana Territory" as the land was bought from France for fifteen million, making each acre roughly 4 cents.[7] The oldest real estate brokerage firm was established in 1855 in Chicago, Illinois, and was initially known as "L. D. Olmsted & Co." but is now known as "Baird & Warner".[8] In 1908, the National Association of Realtors was founded in Chicago and in 1916, the name was changed to the National Association of Real Estate Boards and this was also when the term "realtor" was coined to identify real estate professionals
+      </p>
+      <p className='mb-4 text-slate-700'>
+      The stock market crash of 1929 and the Great Depression in the U.S. caused a major drop in real estate worth and prices and ultimately resulted in depreciation of 50% for the four years after 1929.[10] Housing financing in the U.S. was greatly affected by the Banking Act of 1933 and the National Housing Act in 1934 because it allowed for mortgage insurance for home buyers and this system was implemented by the Federal Deposit Insurance as well as the Federal Housing Administration.[11] In 1938, an amendment was made to the National Housing Act and Fannie Mae, a government agency, was established to serve as a secondary market for mortgages and to give lenders more money in order for new homes to be funded.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      Real estate is property consisting of land and the buildings on it, along with its natural resources such as growing crops (e.g. timber), minerals or water, and wild animals; immovable property of this nature; an interest vested in this (also) an item of real property, (more generally) buildings or housing in general.[1][2] In terms of law, real relates to land property and is different from personal property while estate means the "interest" a person has in that land property
+      </p>
+    </div>
   )
 }
 
